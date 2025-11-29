af69850fcab6d7dc1070d3e3967d43c4343f0c8b
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-11-29 10:48:54 -0700

    initial setup

---
+++ src/components/Tabbed.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import TabContent from "./TabContent.jsx";
+import Tab from "./Tab.jsx";
+import DifferentContent from "./DifferentContent.jsx";
+
+export default function Tabbed({ content }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div>
+      <div className="tabs">
+        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+      </div>
+
+      {activeTab <= 2 ? (
+        <TabContent item={content.at(activeTab)} />
+      ) : (
+        <DifferentContent />
+      )}
+    </div>
+  );
+}
